71b06427eeb93a0c200ca82b919b1c82a5b6e9f3
Adametsofficial <av.styledev@gmail.com>
2020-11-29 01:12:29 +0200

    success: registartion controller

--- src/routes/api/v1/auth/index.js
+++ src/routes/api/v1/auth/index.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
-const registerController = require('../../../../controllers/auth/register.controller');
-const loginController = require('../../../../controllers/auth/login.controller');
-const resetPasswordController = require('../../../../controllers/auth/resetPassword.controller');
+// path to root
+const p2r = '../../../../';
+
+// validation
+const {
+  validateRegistrationBody,
+  validateLoginBody,
+} = require(`${p2r}middlewares/validateBody.middleware`);
+
+// controllers
+const registerController = require(`${p2r}controllers/auth/register.controller`);
+const loginController = require(`${p2r}controllers/auth/login.controller`);
+const resetPasswordController = require(`${p2r}controllers/auth/resetPassword.controller`);
+const autoLoginController = require(`${p2r}controllers/auth/autoLogin.controller`);
+
+router.post('/register', validateRegistrationBody(), registerController);
 
 router.post('/register', registerController);
 
