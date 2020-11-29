10df762f9ae6de2bcef783a8f33066a6272127df
Adametsofficial <av.styledev@gmail.com>
2020-11-29 23:44:32 +0200

    finish: finish realize authenticate

--- src/routes/api/v1/auth/index.js
+++ src/routes/api/v1/auth/index.js
@@ -1,25 +1,37 @@
 const router = require('express').Router();
-// path to root
-const p2r = '../../../../';
 
 // validation
 const {
   validateRegistrationBody,
   validateLoginBody,
-} = require(`${p2r}middlewares/validateBody.middleware`);
+  emailValidator,
+  repeatPasswordValidator,
+} = require('../../../../middlewares/validateBody.middleware');
 
 // controllers
-const registerController = require(`${p2r}controllers/auth/register.controller`);
-const loginController = require(`${p2r}controllers/auth/login.controller`);
-const resetPasswordController = require(`${p2r}controllers/auth/resetPassword.controller`);
-const autoLoginController = require(`${p2r}controllers/auth/autoLogin.controller`);
+const registerController = require('../../../../controllers/auth/register.controller');
+const loginController = require('../../../../controllers/auth/login.controller');
+const {
+  checkUserExist,
+  resetPassword,
+} = require('../../../../controllers/auth/resetPassword.controller');
+const autoLoginController = require('../../../../controllers/auth/autoLogin.controller');
 
 router.post('/register', validateRegistrationBody(), registerController);
 
-router.post('/register', registerController);
+router.post('/login', validateLoginBody(), loginController);
 
-router.post('/login', loginController);
+router.post('/autoLogin', autoLoginController);
 
-router.post('/resetPassword', resetPasswordController);
+router.post('/logout', (req, res) => {
+  res.cookie('refreshToken', null);
+  res.cookie('accessToken', null);
+
+  res.json({ isLogout: true });
+});
+
+router.post('/resetpassword/checkuserexist', [emailValidator], checkUserExist);
+
+router.post('/resetpassword', [repeatPasswordValidator], resetPassword);
 
 module.exports = router;
