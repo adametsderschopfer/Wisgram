d6ef0dc126ac894b9331cba969e19262ec11dc4c
Adametsofficial <av.styledev@gmail.com>
2020-12-01 23:42:04 +0200

    docs: doc for set method

--- server/services/Cache.service.js
+++ server/services/Cache.service.js
@@ -2,8 +2,19 @@ const { redisClient: redis } = require('../server');
 
 class Cache {
 
-  async set() {}
-  
+    next();
+  }
+
+
+  /**
+   *
+   * @param {string} key
+   * @param {string} value
+   * @param {number} seconds
+   *
+   * @returns {Promise<boolean>}
+   */
+
 }
 
 module.exports = new Cache();
