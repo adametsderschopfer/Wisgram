c18de4a0c0d0e4f3edd2e99a2ac2511f80ade594
Adametsofficial <av.styledev@gmail.com>
2020-12-01 09:54:03 +0200

    docs: doc query function

--- server/utils/database.js
+++ server/utils/database.js
@@ -3,6 +3,18 @@ const { mysqlConnectConfig } = require('./config');
 
 const connection = mysql.createPool(mysqlConnectConfig);
 
+/**
+ * A Query string
+ * @typedef {string} QueryString
+ */
+
+/**
+ *
+ * @param {QueryString} _query
+ * @param {Array<string | number>} values
+ * @return {Promise<Array<string | number | object>>}
+ */
+
 function query(_query, values) {
   return new Promise((resolve, reject) => {
     connection.query(_query, values, (err, result) => {
